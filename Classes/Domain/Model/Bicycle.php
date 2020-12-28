ee30336d304af62b1fa074d1dc96299e85a9dbc8
Christian Pschorr <c.pschorr@datamints.com>
2020-12-28 13:45:08 +0100

    Mapping von den Feldern angepasst und Model um Token erweitert

--- Classes/Domain/Model/Bicycle.php
+++ Classes/Domain/Model/Bicycle.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace Ps\EntityBicycleMarket\Domain\Model;
 
-
 /***
  *
  * This file is part of the "Bicycle Market" Extension for TYPO3 CMS.
@@ -14,281 +13,282 @@ namespace Ps\EntityBicycleMarket\Domain\Model;
  *  (c) 2020 Christian Pschorr <pschorr.christian@gmail.com>
  *
  ***/
+
 /**
  * Bicycle
  */
-class Bicycle extends \Ps\Entity\Domain\Model\Entity
-{
+class Bicycle extends \Ps\Entity\Domain\Model\Entity {
 
-    /**
-     * brand
-     * 
-     * @var string
-     */
-    protected $brand = '';
+	/**
+	 * brand
+	 *
+	 * @var string
+	 */
+	protected $brand = '';
 
-    /**
-     * condition
-     * 
-     * @var int
-     */
-    protected $condition = 0;
+	/**
+	 * condition
+	 *
+	 * @var string
+	 */
+	protected $condition = '';
 
-    /**
-     * conditionDescription
-     * 
-     * @var string
-     */
-    protected $conditionDescription = '';
+	/**
+	 * conditionDescription
+	 *
+	 * @var string
+	 */
+	protected $conditionDescription = '';
 
-    /**
-     * serviceCondition
-     * 
-     * @var string
-     */
-    protected $serviceCondition = '';
+	/**
+	 * serviceCondition
+	 *
+	 * @var string
+	 */
+	protected $serviceCondition = '';
 
-    /**
-     * frameSize
-     * 
-     * @var int
-     */
-    protected $frameSize = 0;
+	/**
+	 * frameSize
+	 *
+	 * @var int
+	 */
+	protected $frameSize = 0;
 
-    /**
-     * wheelSize
-     * 
-     * @var int
-     */
-    protected $wheelSize = 0;
+	/**
+	 * wheelSize
+	 *
+	 * @var int
+	 */
+	protected $wheelSize = 0;
 
-    /**
-     * price
-     * 
-     * @var float
-     */
-    protected $price = 0.0;
+	/**
+	 * price
+	 *
+	 * @var float
+	 */
+	protected $price = 0.0;
 
-    /**
-     * priceNegotiationBasis
-     * 
-     * @var bool
-     */
-    protected $priceNegotiationBasis = false;
+	/**
+	 * priceNegotiationBasis
+	 *
+	 * @var bool
+	 */
+	protected $priceNegotiationBasis = false;
 
-    /**
-     * manufacturerGuarantee
-     * 
-     * @var bool
-     */
-    protected $manufacturerGuarantee = false;
+	/**
+	 * manufacturerGuarantee
+	 *
+	 * @var bool
+	 */
+	protected $manufacturerGuarantee = false;
 
-    /**
-     * Returns the brand
-     * 
-     * @return string $brand
-     */
-    public function getBrand()
-    {
-        return $this->brand;
-    }
+	/**
+	 * token
+	 *
+	 * @var string
+	 */
+	protected $token = '';
 
-    /**
-     * Sets the brand
-     * 
-     * @param string $brand
-     * @return void
-     */
-    public function setBrand($brand)
-    {
-        $this->brand = $brand;
-    }
+	/**
+	 * Returns the brand
+	 *
+	 * @return string $brand
+	 */
+	public function getBrand() {
+		return $this->brand;
+	}
 
-    /**
-     * Returns the condition
-     * 
-     * @return int $condition
-     */
-    public function getCondition()
-    {
-        return $this->condition;
-    }
+	/**
+	 * Sets the brand
+	 *
+	 * @param string $brand
+	 * @return void
+	 */
+	public function setBrand($brand) {
+		$this->brand = $brand;
+	}
 
-    /**
-     * Sets the condition
-     * 
-     * @param int $condition
-     * @return void
-     */
-    public function setCondition($condition)
-    {
-        $this->condition = $condition;
-    }
+	/**
+	 * Returns the condition
+	 *
+	 * @return string $condition
+	 */
+	public function getCondition() {
+		return $this->condition;
+	}
 
-    /**
-     * Returns the conditionDescription
-     * 
-     * @return string $conditionDescription
-     */
-    public function getConditionDescription()
-    {
-        return $this->conditionDescription;
-    }
+	/**
+	 * Sets the condition
+	 *
+	 * @param string $condition
+	 * @return void
+	 */
+	public function setCondition($condition) {
+		$this->condition = $condition;
+	}
 
-    /**
-     * Sets the conditionDescription
-     * 
-     * @param string $conditionDescription
-     * @return void
-     */
-    public function setConditionDescription($conditionDescription)
-    {
-        $this->conditionDescription = $conditionDescription;
-    }
+	/**
+	 * Returns the conditionDescription
+	 *
+	 * @return string $conditionDescription
+	 */
+	public function getConditionDescription() {
+		return $this->conditionDescription;
+	}
 
-    /**
-     * Returns the serviceCondition
-     * 
-     * @return string $serviceCondition
-     */
-    public function getServiceCondition()
-    {
-        return $this->serviceCondition;
-    }
+	/**
+	 * Sets the conditionDescription
+	 *
+	 * @param string $conditionDescription
+	 * @return void
+	 */
+	public function setConditionDescription($conditionDescription) {
+		$this->conditionDescription = $conditionDescription;
+	}
 
-    /**
-     * Sets the serviceCondition
-     * 
-     * @param string $serviceCondition
-     * @return void
-     */
-    public function setServiceCondition($serviceCondition)
-    {
-        $this->serviceCondition = $serviceCondition;
-    }
+	/**
+	 * Returns the serviceCondition
+	 *
+	 * @return string $serviceCondition
+	 */
+	public function getServiceCondition() {
+		return $this->serviceCondition;
+	}
 
-    /**
-     * Returns the frameSize
-     * 
-     * @return int $frameSize
-     */
-    public function getFrameSize()
-    {
-        return $this->frameSize;
-    }
+	/**
+	 * Sets the serviceCondition
+	 *
+	 * @param string $serviceCondition
+	 * @return void
+	 */
+	public function setServiceCondition($serviceCondition) {
+		$this->serviceCondition = $serviceCondition;
+	}
 
-    /**
-     * Sets the frameSize
-     * 
-     * @param int $frameSize
-     * @return void
-     */
-    public function setFrameSize($frameSize)
-    {
-        $this->frameSize = $frameSize;
-    }
+	/**
+	 * Returns the frameSize
+	 *
+	 * @return int $frameSize
+	 */
+	public function getFrameSize() {
+		return $this->frameSize;
+	}
 
-    /**
-     * Returns the wheelSize
-     * 
-     * @return int $wheelSize
-     */
-    public function getWheelSize()
-    {
-        return $this->wheelSize;
-    }
+	/**
+	 * Sets the frameSize
+	 *
+	 * @param int $frameSize
+	 * @return void
+	 */
+	public function setFrameSize($frameSize) {
+		$this->frameSize = $frameSize;
+	}
 
-    /**
-     * Sets the wheelSize
-     * 
-     * @param int $wheelSize
-     * @return void
-     */
-    public function setWheelSize($wheelSize)
-    {
-        $this->wheelSize = $wheelSize;
-    }
+	/**
+	 * Returns the wheelSize
+	 *
+	 * @return int $wheelSize
+	 */
+	public function getWheelSize() {
+		return $this->wheelSize;
+	}
 
-    /**
-     * Returns the price
-     * 
-     * @return float $price
-     */
-    public function getPrice()
-    {
-        return $this->price;
-    }
+	/**
+	 * Sets the wheelSize
+	 *
+	 * @param int $wheelSize
+	 * @return void
+	 */
+	public function setWheelSize($wheelSize) {
+		$this->wheelSize = $wheelSize;
+	}
 
-    /**
-     * Sets the price
-     * 
-     * @param float $price
-     * @return void
-     */
-    public function setPrice($price)
-    {
-        $this->price = $price;
-    }
+	/**
+	 * Returns the price
+	 *
+	 * @return float $price
+	 */
+	public function getPrice() {
+		return $this->price;
+	}
 
-    /**
-     * Returns the priceNegotiationBasis
-     * 
-     * @return bool $priceNegotiationBasis
-     */
-    public function getPriceNegotiationBasis()
-    {
-        return $this->priceNegotiationBasis;
-    }
+	/**
+	 * Sets the price
+	 *
+	 * @param float $price
+	 * @return void
+	 */
+	public function setPrice($price) {
+		$this->price = $price;
+	}
 
-    /**
-     * Sets the priceNegotiationBasis
-     * 
-     * @param bool $priceNegotiationBasis
-     * @return void
-     */
-    public function setPriceNegotiationBasis($priceNegotiationBasis)
-    {
-        $this->priceNegotiationBasis = $priceNegotiationBasis;
-    }
+	/**
+	 * Returns the priceNegotiationBasis
+	 *
+	 * @return bool $priceNegotiationBasis
+	 */
+	public function getPriceNegotiationBasis() {
+		return $this->priceNegotiationBasis;
+	}
 
-    /**
-     * Returns the boolean state of priceNegotiationBasis
-     * 
-     * @return bool
-     */
-    public function isPriceNegotiationBasis()
-    {
-        return $this->priceNegotiationBasis;
-    }
+	/**
+	 * Sets the priceNegotiationBasis
+	 *
+	 * @param bool $priceNegotiationBasis
+	 * @return void
+	 */
+	public function setPriceNegotiationBasis($priceNegotiationBasis) {
+		$this->priceNegotiationBasis = $priceNegotiationBasis;
+	}
 
-    /**
-     * Returns the manufacturerGuarantee
-     * 
-     * @return bool $manufacturerGuarantee
-     */
-    public function getManufacturerGuarantee()
-    {
-        return $this->manufacturerGuarantee;
-    }
+	/**
+	 * Returns the boolean state of priceNegotiationBasis
+	 *
+	 * @return bool
+	 */
+	public function isPriceNegotiationBasis() {
+		return $this->priceNegotiationBasis;
+	}
 
-    /**
-     * Sets the manufacturerGuarantee
-     * 
-     * @param bool $manufacturerGuarantee
-     * @return void
-     */
-    public function setManufacturerGuarantee($manufacturerGuarantee)
-    {
-        $this->manufacturerGuarantee = $manufacturerGuarantee;
-    }
+	/**
+	 * Returns the manufacturerGuarantee
+	 *
+	 * @return bool $manufacturerGuarantee
+	 */
+	public function getManufacturerGuarantee() {
+		return $this->manufacturerGuarantee;
+	}
 
-    /**
-     * Returns the boolean state of manufacturerGuarantee
-     * 
-     * @return bool
-     */
-    public function isManufacturerGuarantee()
-    {
-        return $this->manufacturerGuarantee;
-    }
+	/**
+	 * Sets the manufacturerGuarantee
+	 *
+	 * @param bool $manufacturerGuarantee
+	 * @return void
+	 */
+	public function setManufacturerGuarantee($manufacturerGuarantee) {
+		$this->manufacturerGuarantee = $manufacturerGuarantee;
+	}
+
+	/**
+	 * Returns the boolean state of manufacturerGuarantee
+	 *
+	 * @return bool
+	 */
+	public function isManufacturerGuarantee() {
+		return $this->manufacturerGuarantee;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getToken(): string {
+		return $this->token;
+	}
+
+	/**
+	 * @param string $token
+	 */
+	public function setToken(string $token): void {
+		$this->token = $token;
+	}
 }
--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -1,13 +1,40 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 return [
-    \Ps\EntityBicycleMarket\Domain\Model\Bicycle::class => [
-        'tableName' => 'tx_entity_domain_model_entity',
-        'properties' => [
-           'condition' => [
-                'fieldName' => 'tx_entitybicyclemarket_condition'
-            ],
-        ]
-    ],
+	\Ps\EntityBicycleMarket\Domain\Model\Bicycle::class => [
+		'tableName' => 'tx_entity_domain_model_entity',
+		'properties' => [
+			'brand' => [
+				'fieldName' => 'tx_entitybicyclemarket_brand'
+			],
+			'condition' => [
+				'fieldName' => 'tx_entitybicyclemarket_condition'
+			],
+			'conditionDescription' => [
+				'fieldName' => 'tx_entitybicyclemarket_condition_description'
+			],
+			'serviceCondition' => [
+				'fieldName' => 'tx_entitybicyclemarket_service_condition'
+			],
+			'frameSize' => [
+				'fieldName' => 'tx_entitybicyclemarket_frame_size'
+			],
+			'wheelSize' => [
+				'fieldName' => 'tx_entitybicyclemarket_wheel_size'
+			],
+			'priceNegotiationBasis' => [
+				'fieldName' => 'tx_entitybicyclemarket_price_negotiation_basis'
+			],
+			'price' => [
+				'fieldName' => 'tx_entitybicyclemarket_price'
+			],
+			'manufacturerGarantee' => [
+				'fieldName' => 'tx_entitybicyclemarket_manufacturer_guarantee'
+			],
+			'token' => [
+				'fieldName' => 'tx_entitybicyclemarket_token'
+			],
+		]
+	],
 ];
