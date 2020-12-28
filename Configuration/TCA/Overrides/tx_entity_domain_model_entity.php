ccfc70c4bbcc61131396c8a6dec90a6be0d86ed8
Christian Pschorr <c.pschorr@datamints.com>
2020-12-28 13:26:43 +0100

    TCA um die benötigten Felder erweitert und Felder konfiguriert

--- Configuration/TCA/Overrides/tx_entity_domain_model_entity.php
+++ Configuration/TCA/Overrides/tx_entity_domain_model_entity.php
@@ -1,157 +1,170 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-
-if (!isset($GLOBALS['TCA']['tx_entity_domain_model_entity']['ctrl']['type'])) {
-    // no type field defined, so we define it here. This will only happen the first time the extension is installed!!
-    $GLOBALS['TCA']['tx_entity_domain_model_entity']['ctrl']['type'] = 'tx_extbase_type';
-    $tempColumnstx_entitybicyclemarket_tx_entity_domain_model_entity = [];
-    $tempColumnstx_entitybicyclemarket_tx_entity_domain_model_entity[$GLOBALS['TCA']['tx_entity_domain_model_entity']['ctrl']['type']] = [
-        'exclude' => true,
-        'label'   => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_db.xlf:tx_entitybicyclemarket.tx_extbase_type',
-        'config' => [
-            'type' => 'select',
-            'renderType' => 'selectSingle',
-            'items' => [
-                ['',''],
-                ['Bicycle','Tx_EntityBicycleMarket_Bicycle']
-            ],
-            'default' => 'Tx_EntityBicycleMarket_Bicycle',
-            'size' => 1,
-            'maxitems' => 1,
-        ]
-    ];
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_entity_domain_model_entity', $tempColumnstx_entitybicyclemarket_tx_entity_domain_model_entity);
+// ---------------------------------------------------------------------------------------------------------------------
+// Neuer Extbase-Typ
+if(isset($GLOBALS['TCA']['tx_entity_domain_model_entity']['columns']['tx_extbase_type']) === true) {
+	$GLOBALS['TCA']['tx_entity_domain_model_entity']['columns']['tx_extbase_type']['config']['items'][] = ['LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.extbase_type', 'Ps\EntityProduct\Domain\Model\Bicycle'];
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-    'tx_entity_domain_model_entity',
-    $GLOBALS['TCA']['tx_entity_domain_model_entity']['ctrl']['type'],
-    '',
-    'after:' . $GLOBALS['TCA']['tx_entity_domain_model_entity']['ctrl']['label']
-);
+// ---------------------------------------------------------------------------------------------------------------------
+// Neue Paletten
+$GLOBALS['TCA']['tx_entity_domain_model_entity']['palettes']['bicycle_general'] = [
+	'showitem' => 'title, --linebreak--, tx_entitybicyclemarket_brand, --linebreak--, long_description, --linebreak--, media, '
+];
 
+$GLOBALS['TCA']['tx_entity_domain_model_entity']['palettes']['bicycle_condition'] = [
+	'showitem' => 'tx_entitybicyclemarket_condition, --linebreak--, tx_entitybicyclemarket_condition_description,'
+];
 
+$GLOBALS['TCA']['tx_entity_domain_model_entity']['palettes']['bicycle_size'] = [
+	'showitem' => 'tx_entitybicyclemarket_frame_size, tx_entitybicyclemarket_wheel_size,'
+];
 
+$GLOBALS['TCA']['tx_entity_domain_model_entity']['palettes']['bicycle_price'] = [
+	'showitem' => 'tx_entitybicyclemarket_price_negotiation_basis, tx_entitybicyclemarket_price,'
+];
 
-$tmp_entity_bicycle_market_columns = [
-
-
-    'brand' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_db.xlf:tx_entitybicyclemarket_domain_model_bicycle.brand',
-        'config' => [
-            'type' => 'input',
-            'size' => 30,
-            'eval' => 'trim'
-        ],
-    ],
-    'tx_entitybicyclemarket_condition' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_db.xlf:tx_entitybicyclemarket_domain_model_bicycle.tx_entitybicyclemarket_condition',
-        'config' => [
-            'type' => 'select',
-            'renderType' => 'selectSingle',
-            'items' => [
-                ['-- Label --', 0],
-            ],
-            'size' => 1,
-            'maxitems' => 1,
-            'eval' => ''
-        ],
-    ],
-    'condition_description' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_db.xlf:tx_entitybicyclemarket_domain_model_bicycle.condition_description',
-        'config' => [
-            'type' => 'text',
-            'cols' => 40,
-            'rows' => 15,
-            'eval' => 'trim'
-        ]
-    ],
-    'service_condition' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_db.xlf:tx_entitybicyclemarket_domain_model_bicycle.service_condition',
-        'config' => [
-            'type' => 'input',
-            'size' => 30,
-            'eval' => 'trim'
-        ],
-    ],
-    'frame_size' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_db.xlf:tx_entitybicyclemarket_domain_model_bicycle.frame_size',
-        'config' => [
-            'type' => 'input',
-            'size' => 4,
-            'eval' => 'int'
-        ]
-    ],
-    'wheel_size' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_db.xlf:tx_entitybicyclemarket_domain_model_bicycle.wheel_size',
-        'config' => [
-            'type' => 'input',
-            'size' => 4,
-            'eval' => 'int'
-        ]
-    ],
-    'price' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_db.xlf:tx_entitybicyclemarket_domain_model_bicycle.price',
-        'config' => [
-            'type' => 'input',
-            'size' => 30,
-            'eval' => 'double2'
-        ]
-    ],
-    'price_negotiation_basis' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_db.xlf:tx_entitybicyclemarket_domain_model_bicycle.price_negotiation_basis',
-        'config' => [
-            'type' => 'check',
-            'items' => [
-                '1' => [
-                    '0' => 'LLL:EXT:lang/locallang_core.xlf:labels.enabled'
-                ]
-            ],
-            'default' => 0,
-        ]
-    ],
-    'manufacturer_guarantee' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_db.xlf:tx_entitybicyclemarket_domain_model_bicycle.manufacturer_guarantee',
-        'config' => [
-            'type' => 'check',
-            'items' => [
-                '1' => [
-                    '0' => 'LLL:EXT:lang/locallang_core.xlf:labels.enabled'
-                ]
-            ],
-            'default' => 0,
-        ]
-    ],
-
+$GLOBALS['TCA']['tx_entity_domain_model_entity']['palettes']['bicycle_guarantee'] = [
+	'showitem' => 'tx_entitybicyclemarket_manufacturer_guarantee,'
 ];
 
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_entity_domain_model_entity',$tmp_entity_bicycle_market_columns);
+$GLOBALS['TCA']['tx_entity_domain_model_entity']['palettes']['bicycle_token'] = [
+	'showitem' => 'tx_entitybicyclemarket_token,'
+];
 
+$GLOBALS['TCA']['tx_entity_domain_model_entity']['palettes']['bicycle_hidden'] = [
+	'showitem' => 'language, extended, slug',
+	'isHiddenPalette' => 1
+];
 
-/* inherit and extend the show items from the parent class */
+// ---------------------------------------------------------------------------------------------------------------------
+// Neue Felder
+$tmpEntityBicycleMarketColumns = [
+	'tx_entitybicyclemarket_brand' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.brand',
+		'config' => [
+			'type' => 'input',
+			'size' => 40,
+			'eval' => 'trim'
+		],
+	],
+	'tx_entitybicyclemarket_condition' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.tx_entitybicyclemarket_condition',
+		'config' => [
+			'type' => 'select',
+			'renderType' => 'selectSingle',
+			'items' => [
+				['', ''],
+				['LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.tx_entitybicyclemarket_condition.new', 'new'],
+				['LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.tx_entitybicyclemarket_condition.as_new', 'asNew'],
+				['LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.tx_entitybicyclemarket_condition.used', 'used'],
+			],
+			'size' => 1,
+			'maxitems' => 1,
+			'eval' => ''
+		],
+	],
+	'tx_entitybicyclemarket_condition_description' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.condition_description',
+		'config' => [
+			'type' => 'text',
+			'cols' => 40,
+			'rows' => 4,
+			'eval' => 'trim'
+		]
+	],
+	'tx_entitybicyclemarket_service_condition' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.service_condition',
+		'config' => [
+			'type' => 'input',
+			'size' => 40,
+			'eval' => 'trim'
+		],
+	],
+	'tx_entitybicyclemarket_frame_size' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.frame_size',
+		'config' => [
+			'type' => 'input',
+			'size' => 4,
+			'eval' => 'int'
+		]
+	],
+	'tx_entitybicyclemarket_wheel_size' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.wheel_size',
+		'config' => [
+			'type' => 'input',
+			'size' => 4,
+			'eval' => 'int'
+		]
+	],
+	'tx_entitybicyclemarket_price' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.price',
+		'config' => [
+			'type' => 'input',
+			'size' => 40,
+			'eval' => 'double2'
+		],
+		'displayCond' => 'FIELD:tx_entitybicyclemarket_price_negotiation_basis:!=:1',
+	],
+	'tx_entitybicyclemarket_price_negotiation_basis' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.price_negotiation_basis',
+		'config' => [
+			'type' => 'check',
+			'items' => [
+				'1' => [
+					'0' => 'LLL:EXT:lang/locallang_core.xlf:labels.enabled'
+				]
+			],
+			'default' => 0,
+		],
+		'onChange' => 'reload',
+	],
+	'tx_entitybicyclemarket_manufacturer_guarantee' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.manufacturer_guarantee',
+		'config' => [
+			'type' => 'check',
+			'items' => [
+				'1' => [
+					'0' => 'LLL:EXT:lang/locallang_core.xlf:labels.enabled'
+				]
+			],
+			'default' => 0,
+		]
+	],
+	'tx_entitybicyclemarket_token' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_tca.xlf:tx_entitybicyclemarket_domain_model_bicycle.token',
+		'config' => [
+			'type' => 'input',
+			'size' => 40,
+			'eval' => 'trim',
+			'readOnly' => 1
+		],
+	],
+];
 
-if (isset($GLOBALS['TCA']['tx_entity_domain_model_entity']['types']['1']['showitem'])) {
-    $GLOBALS['TCA']['tx_entity_domain_model_entity']['types']['Tx_EntityBicycleMarket_Bicycle']['showitem'] = $GLOBALS['TCA']['tx_entity_domain_model_entity']['types']['1']['showitem'];
-} elseif(is_array($GLOBALS['TCA']['tx_entity_domain_model_entity']['types'])) {
-    // use first entry in types array
-    $tx_entity_domain_model_entity_type_definition = reset($GLOBALS['TCA']['tx_entity_domain_model_entity']['types']);
-    $GLOBALS['TCA']['tx_entity_domain_model_entity']['types']['Tx_EntityBicycleMarket_Bicycle']['showitem'] = $tx_entity_domain_model_entity_type_definition['showitem'];
-} else {
-    $GLOBALS['TCA']['tx_entity_domain_model_entity']['types']['Tx_EntityBicycleMarket_Bicycle']['showitem'] = '';
-}
-$GLOBALS['TCA']['tx_entity_domain_model_entity']['types']['Tx_EntityBicycleMarket_Bicycle']['showitem'] .= ',--div--;LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_db.xlf:tx_entitybicyclemarket_domain_model_bicycle,';
-$GLOBALS['TCA']['tx_entity_domain_model_entity']['types']['Tx_EntityBicycleMarket_Bicycle']['showitem'] .= 'brand, tx_entitybicyclemarket_condition, condition_description, service_condition, frame_size, wheel_size, price, price_negotiation_basis, manufacturer_guarantee';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_entity_domain_model_entity', $tmpEntityBicycleMarketColumns);
 
-
-$GLOBALS['TCA']['tx_entity_domain_model_entity']['columns'][$GLOBALS['TCA']['tx_entity_domain_model_entity']['ctrl']['type']]['config']['items'][] = ['LLL:EXT:entity_bicycle_market/Resources/Private/Language/locallang_db.xlf:tx_entity_domain_model_entity.tx_extbase_type.Tx_EntityBicycleMarket_Bicycle','Tx_EntityBicycleMarket_Bicycle'];
+$GLOBALS['TCA']['tx_entity_domain_model_entity']['types']['Ps\EntityProduct\Domain\Model\Bicycle']['showitem'] = '
+--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+	--palette--;;bicycle_general,
+	--palette--;;bicycle_condition,
+	--palette--;;bicycle_size,
+	--palette--;;bicycle_price,
+	--palette--;;bicycle_guarantee,
+	--palette--;;bicycle_token,
+	--palette--;;bicycle_hidden,
+--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+	--palette--;;access,
+';
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,15 +1,12 @@
 CREATE TABLE tx_entity_domain_model_entity (
-
-
-	brand varchar(255) DEFAULT '' NOT NULL,
-	tx_entitybicyclemarket_condition int(11) DEFAULT '0' NOT NULL,
-	condition_description text,
-	service_condition varchar(255) DEFAULT '' NOT NULL,
-	frame_size int(11) DEFAULT '0' NOT NULL,
-	wheel_size int(11) DEFAULT '0' NOT NULL,
-	price double(11,2) DEFAULT '0.00' NOT NULL,
-	price_negotiation_basis smallint(5) unsigned DEFAULT '0' NOT NULL,
-	manufacturer_guarantee smallint(5) unsigned DEFAULT '0' NOT NULL
-
-
+	tx_entitybicyclemarket_brand varchar(255) DEFAULT '' NOT NULL,
+	tx_entitybicyclemarket_condition varchar(10) DEFAULT '' NOT NULL,
+	tx_entitybicyclemarket_condition_description text,
+	tx_entitybicyclemarket_service_condition varchar(255) DEFAULT '' NOT NULL,
+	tx_entitybicyclemarket_frame_size int(11) DEFAULT '0' NOT NULL,
+	tx_entitybicyclemarket_wheel_size int(11) DEFAULT '0' NOT NULL,
+	tx_entitybicyclemarket_price double(11,2) DEFAULT '0.00' NOT NULL,
+	tx_entitybicyclemarket_price_negotiation_basis smallint(5) unsigned DEFAULT '0' NOT NULL,
+	tx_entitybicyclemarket_manufacturer_guarantee smallint(5) unsigned DEFAULT '0' NOT NULL,
+	tx_entitybicyclemarket_token varchar(10) DEFAULT '' NOT NULL
 );
