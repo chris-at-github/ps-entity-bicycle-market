2647f6e968ed0693fe36ca9532e929afe917a1a8
Christian Pschorr <c.pschorr@datamints.com>
2020-12-30 22:57:49 +0100

    Verlinkung auf die Anfrageseite implementiert + Umbau des PrefillTokenElementService auf das Bicycle Objekt

--- Classes/Service/PrefillTokenElementService.php
+++ Classes/Service/PrefillTokenElementService.php
@@ -2,7 +2,10 @@
 
 namespace Ps\EntityBicycleMarket\Service;
 
+use Ps\EntityBicycleMarket\Domain\Model\Bicycle;
+use Ps\EntityBicycleMarket\Domain\Repository\BicycleRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 class PrefillTokenElementService {
@@ -13,10 +16,19 @@ class PrefillTokenElementService {
 	 */
 	public function initializeFormElement(\TYPO3\CMS\Form\Domain\Model\Renderable\RenderableInterface $renderable) {
 		if($renderable->getIdentifier() === 'token') {
-			$token = GeneralUtility::_GET('token');
+			$request = GeneralUtility::_GET('tx_entitybicyclemarket_frontend');
 
-			if(empty($token) === false) {
-				$renderable->setDefaultValue(preg_replace('/[^0-9a-zA-Z]+/', null, $token));
+			if(empty($request) === false && empty($request['bicycle']) === false) {
+
+				/** @var ObjectManager $objectManager */
+				$objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+
+				/** @var Bicycle $bicycle */
+				$bicycle = $objectManager->get(BicycleRepository::class)->findByUid((int) $request['bicycle']);
+
+				if($bicycle !== null) {
+					$renderable->setDefaultValue($bicycle->getToken());
+				}
 			}
 		}
 	}
